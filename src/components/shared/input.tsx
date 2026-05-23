911b01242dc87f5034fb0b561824d99fd50cb5a7
Nightsay2002 <wingwf57@gmail.com>
2026-05-23 10:55:52 +0800

    Strip leading zeroes from number inputs

--- src/components/shared/input.tsx
+++ src/components/shared/input.tsx
@@ -5,7 +5,19 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
-  ({ className, onFocus, ...props }, ref) => {
+  ({ className, onChange, onFocus, onInput, onKeyDown, onMouseUp, ...props }, ref) => {
+    const shouldClearDefaultZero = (element: HTMLInputElement) => props.type === "number" && element.value === "0";
+    const normalizeNumberInput = (element: HTMLInputElement) => {
+      if (props.type !== "number") {
+        return;
+      }
+
+      const normalized = element.value.replace(/^(-?)0+(?=\d)/, "$1");
+      if (normalized !== element.value) {
+        element.value = normalized;
+      }
+    };
+
     return (
       <input
         ref={ref}
@@ -14,11 +26,41 @@ export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttribute
           className,
         )}
         onFocus={(event) => {
-          if (props.type === "number" && event.currentTarget.value === "0") {
+          if (shouldClearDefaultZero(event.currentTarget)) {
             event.currentTarget.select();
           }
 
           onFocus?.(event);
+        }}
+        onMouseUp={(event) => {
+          onMouseUp?.(event);
+
+          if (!event.defaultPrevented && shouldClearDefaultZero(event.currentTarget)) {
+            event.preventDefault();
+            event.currentTarget.select();
+          }
+        }}
+        onKeyDown={(event) => {
+          onKeyDown?.(event);
+
+          if (
+            !event.defaultPrevented &&
+            shouldClearDefaultZero(event.currentTarget) &&
+            !event.metaKey &&
+            !event.ctrlKey &&
+            !event.altKey &&
+            (/^\d$/.test(event.key) || event.key === "-")
+          ) {
+            event.currentTarget.value = "";
+          }
+        }}
+        onInput={(event) => {
+          normalizeNumberInput(event.currentTarget);
+          onInput?.(event);
+        }}
+        onChange={(event) => {
+          normalizeNumberInput(event.currentTarget);
+          onChange?.(event);
         }}
         {...props}
       />
